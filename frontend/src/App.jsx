388afbe9243245de9b9982498e990585e3f5e914
Vladislav Reshetnikov <barsikus07@gmail.com>
2022-03-07 20:45:09 +0500

    remove tests && added styled to App.jsx && prod compose fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import logo from './logo.svg';
-import './App.css';
+const Main = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <Header>
+        <p>Edit any file in src/ to reload.</p>
+      </Header>
+    </Main>
   );
 }
 
