df16654ee5a15e2425631a96bdb1c65d8517822b
Vladislav <barsikus07@gmail.com>
2022-05-02 15:33:09 +0300

    small fixes and migrations removing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const Header = styled.header`
   background-color: #282c34;
-  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
