0a0e4a704bcc1d8e877aecdd858d6bcdd04913bb
Vladislav Reshetnikov <barsikus07@gmail.com>
2022-06-01 01:54:50 +0400

    cleanup

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -9,9 +9,7 @@ class Settings(BaseSettings):
     DEBUG: bool = False
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    ENCRYPT_KEY: str = secrets.token_urlsafe(32)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 180
     FIRST_SUPERUSER_NAME: str
     FIRST_SUPERUSER_EMAIL: EmailStr
     FIRST_SUPERUSER_PASSWORD: str
--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,8 +1,4 @@
 from .user import User
-from .file import File
-from .image import Image
-# from .user_image import UserImage
-from .user_favorite import UserFavorite
 
 
-__all__ = ["User", "File", "Image", "UserFavorite"]
+__all__ = ["User"]
